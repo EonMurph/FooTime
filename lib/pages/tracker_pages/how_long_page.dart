f546b5e1ed48f04b7ca2174c9fdf67eb3b9743b7
EonMurph <stiger.home@gmail.com>
2025-01-01 02:48:45 +0000

    feat: basic time allocation page

--- lib/pages/tracker_pages/how_long_page.dart
+++ lib/pages/tracker_pages/how_long_page.dart
@@ -1,16 +1,76 @@
 import "package:flutter/material.dart";
+import "package:foo_time/utils/activity.dart";
 
-class HowLongPage extends StatelessWidget {
+class HowLongPage extends StatefulWidget {
   const HowLongPage({super.key});
 
   @override
+  State<HowLongPage> createState() => _HowLongPageState();
+}
+
+class _HowLongPageState extends State<HowLongPage> {
+  final List<double> sliderValues = [0, 0, 0, 0, 0, 0];
+
+  @override
   Widget build(BuildContext context) {
-    return GridView.builder(
-      gridDelegate:
-          SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) {
-        return Placeholder();
-      },
+    final activities = const [
+      Activity(id: 1, name: 'Coding', icon: Icons.cloud),
+      Activity(id: 1, name: 'Coding', icon: Icons.cloud),
+    ];
+    final elapsed = 15;
+    final startTime = '12:45';
+    final endTime = '13:35';
+
+    return Center(
+      child: ListView.builder(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(30),
+        itemCount: activities.length,
+        itemBuilder: (context, index) {
+          return Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                activities[index].icon,
+                size: 50,
+              ),
+              Expanded(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          activities[index].name,
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        Text(
+                          '$elapsed mins',
+                          style: Theme.of(context).textTheme.labelSmall,
+                        ),
+                      ],
+                    ),
+                    Slider(
+                      value: sliderValues[index],
+                      min: 0,
+                      max: 100,
+                      label: sliderValues[index].round().toString(),
+                      onChanged: (double value) {
+                        setState(() {
+                          sliderValues[index] = value;
+                        });
+                      },
+                    ),
+                    Row(children: [
+                      Text(startTime),
+                      Text(endTime),
+                    ])
+                  ],
+                ),
+              )
+            ],
+          );
+        },
+      ),
     );
   }
 }
